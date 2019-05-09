d79a01b95a956d80c2e26d45c645fe89709d869a
Melissa Geels <spansjh@gmail.com>
2019-05-09 17:22:49 +0200

    Avoid aborting when json throws exceptions

--- UniversalNetworking/src/Utils.cpp
+++ UniversalNetworking/src/Utils.cpp
@@ -8,10 +8,18 @@ std::vector<uint8_t> Unet::JsonPack(const json &js)
 
 json Unet::JsonUnpack(const std::vector<uint8_t> &data)
 {
-	return json::from_msgpack(data);
+	try {
+		return json::from_msgpack(data);
+	} catch (json::exception ex) {
+		return json();
+	}
 }
 
 json Unet::JsonUnpack(uint8_t* data, size_t size)
 {
-	return json::from_msgpack(data, size);
+	try {
+		return json::from_msgpack(data, size);
+	} catch (json::exception ex) {
+		return json();
+	}
 }
